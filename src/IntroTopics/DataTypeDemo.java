f4f3a2b549ce1ad1fb8d6b7ed06538ccb7fdd145
Milind Mishra <milind.mishra4@gmail.com>
2023-05-26 01:31:21 +0800

    added data types demo

---
+++ src/IntroTopics/DataTypeDemo.java
@@ -0,0 +1,31 @@
+package IntroTopics;
+
+public class DataTypeDemo {
+    public static void main(String[] args) {
+        boolean flag = true;
+        System.out.println(flag);
+
+        byte range = 124;
+        System.out.println(range);
+
+        short temperature = -200;
+        System.out.println(temperature);
+
+        int ranges = -4250000;
+        System.out.println(ranges);
+
+        long ranges1 = -43234234234L;
+        System.out.println(ranges1);
+
+        double number = -42.4;
+        System.out.println(number);
+
+        float floatVariable =  -6.1f;
+        System.out.println(floatVariable);
+
+        char character = 'c';
+        System.out.println(character);
+
+    }
+
+}
